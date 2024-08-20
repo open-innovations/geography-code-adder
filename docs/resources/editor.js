7ad3eb90ad0d3922025bc651a97d47504ca307aa
slowe <299787+slowe@users.noreply.github.com>
2024-08-20 10:20:59 +0100

    Update ISO8601 icon

--- docs/resources/editor.js
+++ docs/resources/editor.js
@@ -422,7 +422,7 @@
 			}).addButton({
 				'id':'btn-isodate',
 				'class':'c5-bg',
-				'text':'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi" viewBox="0 0 16 16"><path d="M8 5.5m 5.5,0 a 5.5,5.5 0 1,0 -11,0 a 5.5,5.5 0 1,0 11,0M 8 5.5m-0.5 -0.5v-3.5h1v4.5h-4v-1h3z"/><text x="8" y="12" text-anchor="middle" dominant-baseline="hanging" font-size="4" font-family="Poppins">ISO8601</text><title>Convert dates to ISO8601</title></svg>',
+				'text':'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/><text x="8" y="7" text-anchor="middle" dominant-baseline="hanging" font-size="5" font-family="Poppins">8601</text><title>Convert dates to ISO8601</title></svg>',
 				'on':{
 					'click': function(e){
 						_obj.startEdit();
