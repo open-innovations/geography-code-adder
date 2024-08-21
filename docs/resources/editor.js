a4794b876f4d514280d2878cde57316fd9f744ac
slowe <299787+slowe@users.noreply.github.com>
2024-08-21 20:38:38 +0100

    Update button

--- docs/resources/editor.js
+++ docs/resources/editor.js
@@ -476,7 +476,7 @@
 			}).addButton({
 				'id':'btn-isodate',
 				'class':'',
-				'text':'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/><text x="8" y="7" text-anchor="middle" dominant-baseline="hanging" font-size="5" font-family="Poppins">8601</text><title>Convert dates to ISO8601</title></svg>',
+				'text':'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi" viewBox="0 0 16 16"><path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/><text x="8" y="7" text-anchor="middle" dominant-baseline="hanging" font-size="5" font-family="Poppins">Aug</text><title>Convert date format</title></svg>',
 				'on':{
 					'click': function(e){ _obj.startDates(); },
 					'init': function(btn,e){
